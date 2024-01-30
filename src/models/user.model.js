1df6ddfcd7260ebfbfe645e29c5ef5ec9ea12a46
dhruv <dr26dhruv@gmail.com>
2024-01-30 13:22:24 +0530

    userModel

---
+++ src/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      //kisi bhi field vo searchable bana he to index ko true kar do (index should use carefully as it is a expensive operation but it optimizes the seaching and also apply only where seaching is continous)
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary URL
+    },
+    // array
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//timestamps: true = createdAt and updatedAt
+export const User = mongoose.model("User",userSchema)
