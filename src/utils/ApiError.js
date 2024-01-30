a99537174c523526a79d7d13d21683f1ade610b8
dhruv <dr26dhruv@gmail.com>
2024-01-30 16:17:32 +0530

    typo error fixed

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         message = "Something went wrong",
         errors = [],
-        statck= ""
+        stack= ""
     ){
         //override
         super(message)
@@ -13,9 +13,9 @@ class ApiError extends Error{
         this.success =false
         this.errors = errors
 
-        if(statck)
+        if(stack)
         {
-            this.stack= statck
+            this.stack= stack
         }
         else{
             Error.captureStackTrace(this,this.constructor)
