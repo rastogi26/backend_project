42b3b747469baf4a6bb5892e080f7f44df1092f8
dhruv <dr26dhruv@gmail.com>
2024-02-02 20:15:26 +0530

    addition of login and logout routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,32 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from '../middlewares/multer.middleware.js'
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 // injecting multer middleware to handle files in registeration 2 files i.e avatar and cover image
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar", // communication between frontend and backend to have same nae field.
-            maxCount: 1  // how many files to accept
+  upload.fields([
+    {
+      name: "avatar", // communication between frontend and backend to have same nae field.
+      maxCount: 1, // how many files to accept
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+); // https://localhost:8000/api/v1/users/register
 
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser)  // https://localhost:8000/api/v1/users/register
+router.route("/login").post(loginUser);
 
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser); // before logout the user, check if it is login or not by passing a middleware
 
-
-
-export default router
+export default router;
