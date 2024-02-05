ffc4b84b1a76749dfc99d3d8d1c36c7f3288c9f3
dhruv <dr26dhruv@gmail.com>
2024-02-05 20:54:03 +0530

    add new get , patch and post routes in userRoutes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchedHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,5 +37,20 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser); // before logout the user, check if it is login or not by passing a middleware
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post;
+verifyJWT, changeCurrentPassword;
+router.route("/current-user").get(verifyJWT, getCurrentUser); // check if user login or not , if true then getCurrentUser details.
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar); // 1. middleware check user login or not , 2nd middleware is multer
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage); //flag ->coverImage
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); // getting from params and it is defined as username in function so it is named as :username
+
+router.route("/history").get(verifyJWT,getWatchedHistory);
 export default router;
